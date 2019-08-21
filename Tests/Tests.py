760a1a2d76652d336f299f1375914e90c58f1f78
SkyLined <github@skylined.nl>
2019-08-21 12:12:17 +0200

    Add code to check imports

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,11 +1,54 @@
-import os, sys;
+import json, os, sys;
 
-# Augment the search path to make cBugId a package and have access to its modules folder.
-sTestFolderPath = os.path.dirname(os.path.abspath(__file__));
-sMainFolderPath = os.path.dirname(sTestFolderPath);
+# Augment the search path to make the test subject a package and have access to its modules folder.
+sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
+sMainFolderPath = os.path.dirname(sTestsFolderPath);
 sParentFolderPath = os.path.dirname(sMainFolderPath);
 sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
-sys.path = [sTestFolderPath, sMainFolderPath, sParentFolderPath, sModulesFolderPath] + sys.path[:];
+asOriginalSysPath = sys.path[:];
+sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
+# Load product details
+oProductDetailsFile = open(os.path.join(sMainFolderPath, "dxProductDetails.json"), "rb");
+try:
+  dxProductDetails = json.load(oProductDetailsFile);
+finally:
+  oProductDetailsFile.close();
+# Save the list of names of loaded modules:
+asOriginalModuleNames = sys.modules.keys();
+
+__import__(dxProductDetails["sProductName"], globals(), locals(), [], -1);
+
+# Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
+# loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
+# will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
+asUnexpectedModules = list(set([
+  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
+  if not (
+    sModuleName in asOriginalModuleNames # This was loaded before
+    or sModuleName.lstrip("_").split(".", 1)[0] in (
+      [dxProductDetails["sProductName"]] +
+      dxProductDetails["asDependentOnProductNames"] +
+      [
+        # These are optional, not required:
+        "oConsole", "mWindowsSDK",
+        # These built-in modules are expected:
+        "Queue", "StringIO", "base64", "binascii", "cStringIO", "collections",
+        "contextlib", "ctypes", "gc", "gzip", "heapq", "io", "itertools",
+        "json", "keyword", "msvcrt", "nturl2path", "platform", "socket", "ssl",
+        "string", "strop", "struct", "subprocess", "textwrap", "thread",
+        "threading", "time", "urllib", "urlparse", "zlib"
+      ]
+    )
+  )
+]));
+assert len(asUnexpectedModules) == 0, \
+      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
+
+#Import the test subject
+import mHTTP;
+
+# Restore the search path
+sys.path = asOriginalSysPath;
 
 from mDebugOutput import fDebugOutput, fFatalExceptionOutput, fShowFileDebugOutputForClass;
 try:
@@ -21,6 +64,7 @@ try:
   from mHTTP.cSSLContext import cSSLContext;
   from mHTTP.iHTTPMessage import iHTTPMessage;
   from mMultiThreading import cLock;
+
   # Enable/disable output for all classes
 #  fShowFileDebugOutputForClass(cBufferedSocket);
 #  fShowFileDebugOutputForClass(cCertificateAuthority);
