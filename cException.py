7d97a8ef9d480207881970f4dbeba2aa582974b4
SkyLined <github@skylined.nl>
2019-11-13 08:48:20 +0100

    Second argument does not need to be string

--- cException.py
+++ cException.py
@@ -1,10 +1,11 @@
 class cException(Exception):
-  def __init__(oSelf, sMessage, sDetails):
+  def __init__(oSelf, sMessage, xDetails):
     oSelf.sMessage = sMessage;
-    oSelf.sDetails = sDetails;
-    Exception.__init__(oSelf, sMessage, sDetails);
+    oSelf.xDetails = xDetails;
+    Exception.__init__(oSelf, (sMessage, xDetails));
   
   def __repr__(oSelf):
     return "<%s %s>" % (oSelf.__class__.__name__, oSelf);
   def __str__(oSelf):
-    return "%s (%s)" % (oSelf.sMessage, oSelf.sDetails);
+    sDetails = str(oSelf.xDetails) if not hasattr(oSelf.xDetails, "fsToString") else oSelf.xDetails.fsToString();
+    return "%s (%s)" % (oSelf.sMessage, sDetails);
