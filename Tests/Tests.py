c1b95adbd517e5720e9fe9fb64e4b09a4aafc56e
SkyLined <github@skylined.nl>
2020-06-17 11:40:44 +0200

    Add dependency tests

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,65 +1,18 @@
-import json, os, sys;
-
-# Augment the search path to make the test subject a package and have access to its modules folder.
-sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
-sMainFolderPath = os.path.dirname(sTestsFolderPath);
-sParentFolderPath = os.path.dirname(sMainFolderPath);
-sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
-asOriginalSysPath = sys.path[:];
-sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
-# Load product details
-oProductDetailsFile = open(os.path.join(sMainFolderPath, "dxProductDetails.json"), "rb");
-try:
-  dxProductDetails = json.load(oProductDetailsFile);
-finally:
-  oProductDetailsFile.close();
-# Save the list of names of loaded modules:
-asOriginalModuleNames = sys.modules.keys();
-
-__import__(dxProductDetails["sProductName"], globals(), locals(), [], -1);
-
-# Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
-# loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
-# will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
-asUnexpectedModules = list(set([
-  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
-  if not (
-    sModuleName in asOriginalModuleNames # This was loaded before
-    or sModuleName.lstrip("_").split(".", 1)[0] in (
-      [dxProductDetails["sProductName"]] +
-      dxProductDetails["asDependentOnProductNames"] +
-      [
-        # This is optional, not required:
-        "oConsole", 
-        # These built-in modules are expected:
-        "base64", "binascii", "cStringIO", "collections", "contextlib",
-        "ctypes", "datetime", "dis", "future__", "hashlib", "gc", "gzip",
-        "heapq", "imp", "inspect", "io", "itertools", "json", "keyword",
-        "math", "msvcrt", "nturl2path", "opcode", "platform", "random",
-        "Queue", "select", "socket", "ssl", "string", "StringIO", "strop",
-        "struct", "subprocess", "textwrap", "thread", "threading", "time",
-        "timeit", "token", "tokenize", "urllib", "urlparse", "zlib"
-      ]
-    )
-  )
-]));
-assert len(asUnexpectedModules) == 0, \
-      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
-for sModuleName in dxProductDetails["asDependentOnProductNames"]:
-  assert sModuleName in sys.modules, \
-      "%s is listed as a dependency but not loaded by the module!" % sModuleName;
+from fTestDependencies import fTestDependencies;
+fTestDependencies();
 
 from mDebugOutput import fEnableDebugOutputForClass, fEnableDebugOutputForModule, fTerminateWithException;
 try:
+  import os, sys;
   from oConsole import oConsole;
+  from mMultiThreading import cLock;
+  import mSSL;
   
-  import mHTTP, mSSL;
+  import mHTTP;
   
   from fTestClient import fTestClient;
   from fTestServer import fTestServer;
   from fTestProxyClientAndServer import fTestProxyClientAndServer;
-  
-  from mMultiThreading import cLock;
   
   def fLogEvents(oWithCallbacks, sWithCallbacks = None):
     def fAddCallback(sEventName):
@@ -162,7 +115,7 @@ try:
       oConsole.fOutput(" Test HTTP client proxy server%s." % (" with intercepted HTTPS connections" if oCertificateAuthority else ""));
       oConsole.fOutput("\xFE" * 160);
       fTestProxyClientAndServer(oProxyServerURL, oCertificateStore, oCertificateAuthority, nEndWaitTimeoutInSeconds, fzLogEvents);
+  oConsole.fOutput("+ Done.");
 except Exception as oException:
   fTerminateWithException(oException, bShowStacksForAllThread = True);
-else:
-  oConsole.fOutput("+ Done.");
+
