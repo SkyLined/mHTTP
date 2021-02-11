06c69247f93d0d4147c6428f44a01a661249e7b0
SkyLined <github@skylined.nl>
2021-02-11 13:19:28 +0100

    Move __init__ to mHTTP

---
+++ mHTTP.py
@@ -0,0 +1,34 @@
+import os, sys;
+
+from .cHTTPClient import cHTTPClient;
+from .cHTTPClientSideProxyServer import cHTTPClientSideProxyServer;
+from .cHTTPClientUsingProxyServer import cHTTPClientUsingProxyServer;
+from .cHTTPClientUsingAutomaticProxyServer import cHTTPClientUsingAutomaticProxyServer;
+from .cHTTPServer import cHTTPServer;
+from .fs0GetMediaTypeForExtension import fs0GetMediaTypeForExtension;
+from . import mExceptions;
+# Pass down
+from mHTTPConnections import \
+    cHTTPConnection, cHTTPConnectionsToServerPool, cHTTPConnectionAcceptor, \
+    cHTTPHeader, cHTTPHeaders, \
+    cHTTPRequest, cHTTPResponse, \
+    cURL;
+
+__all__ = [
+  "cHTTPClient",
+  "cHTTPClientSideProxyServer",
+  "cHTTPClientUsingProxyServer",
+  "cHTTPClientUsingAutomaticProxyServer",
+  "cHTTPServer",
+  "fs0GetMediaTypeForExtension",
+  "mExceptions",
+  # Pass down from mHTTPConnection
+  "cHTTPConnection",
+  "cHTTPConnectionsToServerPool",
+  "cHTTPConnectionAcceptor",
+  "cHTTPHeader", 
+  "cHTTPHeaders", 
+  "cHTTPRequest",
+  "cHTTPResponse",
+  "cURL",
+];
